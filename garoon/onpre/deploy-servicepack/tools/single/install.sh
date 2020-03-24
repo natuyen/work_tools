786c86549f1495bd9db69df36b6b89796d152895
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-03-24 14:53:26 +0700

    update

--- garoon/onpre/deploy-servicepack/tools/single/install.sh
+++ garoon/onpre/deploy-servicepack/tools/single/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 echo "-----------------Start install Garoon Single ..."
 
-GAROON_VERSION=
+GAROON_VERSION
 GAROON_SP_VERSION
 if [ -f "build.conf"]; then
     source build.conf
     GAROON_VERSION=$PRODUCT_VERSION
 fi
+
+GAROON_SP_VERSION=$GAROON_VERSION
 if [ -f "build_sp.conf"]; then
     source build_sp.conf
     GAROON_SP_VERSION=$VERSION
