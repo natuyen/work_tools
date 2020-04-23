486417d34001d7bb51d6a0c4847b0fff67ad541f
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-04-23 15:40:40 +0700

    update

--- garoon/onpre/deploy-servicepack/tools/largescale/install.sh
+++ garoon/onpre/deploy-servicepack/tools/largescale/install.sh
@@ -6,13 +6,13 @@ export HOSTNAME=localhost
 
 # install Garoon
 #if [ ${UNINSTALL_OLD_GAROON} == true ]; then
-    bash -ex largescale/install-largescale.sh $GAROON_VERSION
+    #bash -ex largescale/install-largescale.sh $GAROON_VERSION
 #fi
 
 # install sp
-if [ $INSTALL_SERVICE_PACK == true -a "$GAROON_SP_VERSION" != "" ]; then
-    bash -ex largescale/install-largescale-sp.sh $GAROON_SP_VERSION
-fi
+#if [ $INSTALL_SERVICE_PACK == true -a "$GAROON_SP_VERSION" != "" ]; then
+#    bash -ex largescale/install-largescale-sp.sh $GAROON_SP_VERSION
+#fi
 #-------------------
 # install service pack
 if [ $INSTALL_SERVICE_PACK == true ]; then
