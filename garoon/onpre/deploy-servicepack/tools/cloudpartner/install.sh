85c1c4f163bebd8bf6e477442189b1343b6abf1e
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-04-23 15:58:35 +0700

    update

--- garoon/onpre/deploy-servicepack/tools/cloudpartner/install.sh
+++ garoon/onpre/deploy-servicepack/tools/cloudpartner/install.sh
@@ -5,7 +5,7 @@ export HOSTNAME=localhost
 
 # install service pack
 if [ $INSTALL_SERVICE_PACK == true ]; then
-    bash -ex cloudpartner/install-cloudpartner.sh $GAROON_VERSION
+    bash -ex cloudpartner/install-cloudpartner.sh $GAROON_VERSION $GAROON_VERSION
     bash -ex cloudpartner/install-cloudpartner-sp.sh $GAROON_SP_VERSION $GIT_BRANCH
 else # install only Garoon
     bash -ex cloudpartner/install-cloudpartner.sh $GAROON_VERSION $GIT_BRANCH
