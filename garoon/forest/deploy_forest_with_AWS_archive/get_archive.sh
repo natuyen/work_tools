3d3b6506bb8cb45396dfca75b793cd48e30ea2ae
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-12-11 16:50:35 +0700

    update AWS

--- garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
+++ garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ARCHIVE_LIST=$1
+BRANCH=$2
+PATTERN=$3
 archives=($(grep -oP '(?<=Key>)[^<]+' "${ARCHIVE_LIST}"))
 
 ARCHIVE_LIST_FILTER=archive_list_filter.txt
@@ -8,4 +10,6 @@ do
   echo "${archives[$i]}" >> ${ARCHIVE_LIST_FILTER}
 done
 
-
+#cat archive_list_filter.txt | grep -E "F20.11-2020-12-10-with-e2e".*"-GRF" | sort | tail -n1
+ARCHIVE_PATH=$(cat ${ARCHIVE_LIST_FILTER} | grep -E "${BRANCH}".*"${PATTERN}" | sort | tail -n1)
+echo "${ARCHIVE_PATH}"
