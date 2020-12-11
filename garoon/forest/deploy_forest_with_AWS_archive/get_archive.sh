46e8ffb71080a1b412e4ae2276e2590c49049560
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-12-11 16:38:28 +0700

    update AWS

--- garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
+++ garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-archives=($(grep -oP '(?<=Key>)[^<]+' "archive_list.txt"))
+ARCHIVE_LIST=$1
+archives=($(grep -oP '(?<=Key>)[^<]+' "${ARCHIVE_LIST}"))
 
+ARCHIVE_LIST_FILTER=archive_list_filter.txt
 for i in ${!archives[*]}
 do
-  echo "$i" "${archives[$i]}"
+  echo "${archives[$i]}" >> ${ARCHIVE_LIST_FILTER}
 done
+
+
