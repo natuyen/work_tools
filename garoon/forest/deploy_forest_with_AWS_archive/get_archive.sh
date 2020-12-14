e03025ef2d49e4dfd95121d31b200278d815f0a1
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-12-14 10:11:30 +0700

    update AWS

--- garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
+++ garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-ARCHIVE_LIST=$1
-BRANCH=$2
-PATTERN=$3
-archives=($(grep -oP '(?<=Key>)[^<]+' "${ARCHIVE_LIST}"))
+#ARCHIVE_LIST=$1
+BRANCH=$1
 
-ARCHIVE_LIST_FILTER=archive_list_filter.txt
-for i in ${!archives[*]}
-do
-  echo "${archives[$i]}" >> ${ARCHIVE_LIST_FILTER}
-done
+# get lastest folder
+ARCHIVE_FOLDER_LIST_URL=https://cybozu-garoon-ci.s3.ap-northeast-1.amazonaws.com/?list-type=2&delimiter=%2F&prefix=archives%2F${BRANCH}%2F
+ARCHIVE_FOLDER_LIST=archive_folder_list.txt
+curl -o ${ARCHIVE_FOLDER_LIST} ${ARCHIVE_FOLDER_LIST_URL}
+ARCHIVE_FOLDER_LASTEST=$(cat ${ARCHIVE_FOLDER_LIST} | grep -"<Prefix>" | sort | tail -n1)
+echo "${ARCHIVE_FOLDER_LASTEST}"
 
-#cat archive_list_filter.txt | grep -E "F20.11-2020-12-10-with-e2e".*"-GRF" | sort | tail -n1
-ARCHIVE_PATH=$(cat ${ARCHIVE_LIST_FILTER} | grep -E "${BRANCH}".*"${PATTERN}" | sort | tail -n1)
-echo "${ARCHIVE_PATH}"
+
+#PATTERN=$3
+#archives=($(grep -oP '(?<=Key>)[^<]+' "${ARCHIVE_LIST}"))
+#
+#ARCHIVE_LIST_FILTER=archive_list_filter.txt
+#for i in ${!archives[*]}
+#do
+#  echo "${archives[$i]}" >> ${ARCHIVE_LIST_FILTER}
+#done
+#
+##cat archive_list_filter.txt | grep -E "F20.11-2020-12-10-with-e2e".*"-GRF" | sort | tail -n1
+#ARCHIVE_PATH=$(cat ${ARCHIVE_LIST_FILTER} | grep -E "${BRANCH}".*"${PATTERN}" | sort | tail -n1)
+#echo "${ARCHIVE_PATH}"
