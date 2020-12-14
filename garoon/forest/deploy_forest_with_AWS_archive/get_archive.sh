7623337b0852c27851f67d334a26365432155b84
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-12-14 10:35:25 +0700

    update AWS

--- garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
+++ garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#ARCHIVE_LIST=$1
 BRANCH=$1
 
 # get lastest folder
@@ -15,14 +14,24 @@ for i in ${!archives[*]}
 do
    ARCHIVE_FOLDER_LASTEST="${archives[$i]}"
 done
-
 echo "${ARCHIVE_FOLDER_LASTEST}"
+
+ARCHIVE_FOLDER_LASTEST_URL="https://cybozu-garoon-ci.s3-ap-northeast-1.amazonaws.com/index.html#${ARCHIVE_FOLDER_LASTEST}"
+ARCHIVE_LIST=archive_list.txt
+curl -o ${ARCHIVE_LIST} "${ARCHIVE_FOLDER_LASTEST_URL}"
+
+ARCHIVE_GAROON
+archives=($(grep -oP '(?<=Key>)[^<]+' "${ARCHIVE_LIST}"))
+for i in ${!archives[*]}
+do
+   ARCHIVE_GAROON="${archives[$i]}"
+done
+echo "${ARCHIVE_GAROON}"
 
 #
 ##cat archive_list_filter.txt | grep -E "F20.11-2020-12-10-with-e2e".*"-GRF" | sort | tail -n1
 #ARCHIVE_PATH=$(cat ${ARCHIVE_LIST_FILTER} | grep -E "${BRANCH}".*"${PATTERN}" | sort | tail -n1)
 #echo "${ARCHIVE_PATH}"
-
 
 #PATTERN=$3
 #archives=($(grep -oP '(?<=Key>)[^<]+' "${ARCHIVE_LIST}"))
