7046ebd424138f6fa2f7e8b8ce392a061e824ccc
Tuyen Nguyen <tuyen-nguyen@cybozu.vn>
2020-12-15 10:00:07 +0700

    update AWS

--- garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
+++ garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BRANCH=$1
 ARCHIVE_FOLDER_LASTEST=${ARCHIVE_FOLDER}
+echo ARCHIVE_FOLDER_LASTEST=${ARCHIVE_FOLDER}
 # get lastest folder
 if [ "${ARCHIVE_FOLDER_LASTEST}" == "" ]; then
     ARCHIVE_FOLDER_LIST_URL="https://cybozu-garoon-ci.s3.ap-northeast-1.amazonaws.com/?list-type=2&delimiter=%2F&prefix=archives%2F${BRANCH}%2F"
