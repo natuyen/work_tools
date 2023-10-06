c8c0e9121fc125a5a13027e84551eda8614fdda8
Nguyen Anh Tuyen <tuyen-nguyen@cybozu.vn>
2023-10-06 16:22:17 +0700

    update code

--- garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
+++ garoon/forest/deploy_forest_with_AWS_archive/get_archive.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eu
 BRANCH=$1
-AWS_URL="https://cybozu-garoon-ci.s3-ap-northeast-1.amazonaws.com";
+AWS_URL="https://cybozu-garoon-ci.s3-ap-northeast-1.amazonaws.com"
 
 # read AWS archives url https://cybozu-garoon-ci.s3-ap-northeast-1.amazonaws.com/index.html#archives/
 ARCHIVE_FOLDER_LIST_URL="${AWS_URL}/?list-type=2&delimiter=%2F&prefix=archives%2F${BRANCH}%2F"
-ARCHIVE_FOLDER_LIST=archive_folder_list.txt
-curl -o ${ARCHIVE_FOLDER_LIST} "${ARCHIVE_FOLDER_LIST_URL}"
+ARCHIVE_FOLDER_LIST_XML=archive_folder_list_xml.txt
+curl -o ${ARCHIVE_FOLDER_LIST_XML} "${ARCHIVE_FOLDER_LIST_URL}"
 
 # get KeyCount
-keycount=($(grep -oP '(?<=KeyCount>)[^<]+' "${ARCHIVE_FOLDER_LIST}"))
+keycount=($(grep -oP '(?<=KeyCount>)[^<]+' "${ARCHIVE_FOLDER_LIST_XML}"))
 # KeyCount = 0 : no archive in AWS branch, ex: index.html#archives/F23.5 no exist
 # use index.html#latest-archives/${BRANCH}/
 if [ $keycount -eq 0 ]; then
@@ -16,9 +16,9 @@ if [ $keycount -eq 0 ]; then
 else # use index.html#archives/ url
     # get laster archive folder
     if [ "${ARCHIVE_FOLDER}" == "" ]; then
-        archives_build_array=($(grep -oP '(?<=Prefix>)[^<]+' "${ARCHIVE_FOLDER_LIST}"))
-        archive_build_date=${archives_build_array[-1]}
-        ARCHIVE_FOLDER_LASTEST_PATH="{AWS_URL}/index.html#archives/${BRANCH}/${archive_build_date}"
+        archives_folder_list=($(grep -oP '(?<=Prefix>)[^<]+' "${ARCHIVE_FOLDER_LIST_XML}"))
+        archives_folder_latest=${archives_folder_list[-1]}
+        ARCHIVE_FOLDER_LASTEST_PATH="${AWS_URL}/index.html#archives/${BRANCH}/${archives_folder_latest}"
 #        for i in ${!archives_build_array[*]}
 #        do
 #            archive_build_date=${archives_build_array[$i]}
@@ -29,12 +29,12 @@ else # use index.html#archives/ url
     fi
 fi
 
-echo $ARCHIVE_FOLDER_LASTEST_PATH
+echo ARCHIVE_FOLDER_LASTEST_PATH: "$ARCHIVE_FOLDER_LASTEST_PATH"
 
 # get archive garoon
-ARCHIVE_FOLDER_LASTEST_URL="https://cybozu-garoon-ci.s3.ap-northeast-1.amazonaws.com/?list-type=2&delimiter=%2F&prefix=${ARCHIVE_FOLDER_LASTEST_PATH}"
+#ARCHIVE_FOLDER_LASTEST_URL="https://cybozu-garoon-ci.s3.ap-northeast-1.amazonaws.com/?list-type=2&delimiter=%2F&prefix=${ARCHIVE_FOLDER_LASTEST_PATH}"
 ARCHIVE_LIST=archive_list.txt
-curl -o ${ARCHIVE_LIST} "${ARCHIVE_FOLDER_LASTEST_URL}"
+curl -o ${ARCHIVE_LIST} "${ARCHIVE_FOLDER_LASTEST_PATH}"
 ARCHIVE_GAROON=
 archives=($(grep -oP '(?<=Key>)[^<]+' "${ARCHIVE_LIST}" | grep 'garoon-GRF'))
 for i in ${!archives[*]}
