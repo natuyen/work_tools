b2dbed93c2129396e3753c4fb4287b7d86e02c80
Nguyen Anh Tuyen <tuyen-nguyen@cybozu.vn>
2024-04-25 09:20:21 +0700

    add soap api test

--- soap-api/cbpapi.sh
+++ soap-api/cbpapi.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+echo "------------------------------------------------------------------------------------------------"
 XmlFile=$1
 ApiUrl=http://localhost:18083/cgi-bin/cbgrn/grn.cgi/cbpapi/schedule/api
-curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@${XmlFile}" -X POST "${ApiUrl}"
+sed -e 's/%USER_NAME%/Administrator/g' ${XmlFile} > XML_REPLACE.xml
+curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@XML_REPLACE.xml" -X POST "${ApiUrl}"
--- soap-api/cbpapi_cloud.sh
+++ soap-api/cbpapi_cloud.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+echo "------------------------------------------------------------------------------------------------"
 XmlFile=$1
 ApiUrl=https://tuyennguyen-dev.cybozu-dev.com/g/cbpapi/schedule/api.csp
-curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@${XmlFile}" -X POST "${ApiUrl}"
+sed -e 's/%USER_NAME%/cybozu/g' ${XmlFile} > XML_REPLACE.xml
+curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@XML_REPLACE.xml" -X POST "${ApiUrl}"
