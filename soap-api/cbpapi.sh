eaad349e1714b8de80440876fd26e0b3ab63110b
Nguyen Anh Tuyen <tuyen-nguyen@cybozu.vn>
2024-05-03 08:28:32 +0700

    add soap api test

--- soap-api/cbpapi.sh
+++ soap-api/cbpapi.sh
@@ -2,5 +2,6 @@
 echo "------------------------------------------------------------------------------------------------"
 XmlFile=$1
 ApiUrl=http://localhost:18083/cgi-bin/cbgrn/grn.cgi/cbpapi/schedule/api
-sed -e 's/%USER_NAME%/Administrator/g' ${XmlFile} > XML_REPLACE.xml
-curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@XML_REPLACE.xml" -X POST "${ApiUrl}"
+sed -e 's/%USER_NAME%/Administrator/g' ${XmlFile} > XML_REQUEST.xml
+curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@XML_REQUEST.xml" -X POST "${ApiUrl}" > XML_RESPONSE.xml
+cat XML_RESPONSE.xml
--- soap-api/cbpapi_cloud.sh
+++ soap-api/cbpapi_cloud.sh
@@ -2,5 +2,5 @@
 echo "------------------------------------------------------------------------------------------------"
 XmlFile=$1
 ApiUrl=https://tuyennguyen-dev.cybozu-dev.com/g/cbpapi/schedule/api.csp
-sed -e 's/%USER_NAME%/cybozu/g' ${XmlFile} > XML_REPLACE.xml
-curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@XML_REPLACE.xml" -X POST "${ApiUrl}"
+sed -e 's/%USER_NAME%/cybozu/g' ${XmlFile} > XML_REQUEST.xml
+curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@XML_REQUEST.xml" -X POST "${ApiUrl}"
