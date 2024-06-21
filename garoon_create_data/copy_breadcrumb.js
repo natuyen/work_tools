851a448a755febb9dc4c1625e8004757e55df26d
Nguyen Anh Tuyen <tuyen-nguyen@cybozu.vn>
2024-06-21 14:37:19 +0700

    copy breadcrumb

--- garoon_create_data/copy_breadcrumb.js
+++ garoon_create_data/copy_breadcrumb.js
@@ -1,19 +1,31 @@
 javascript: (() => {
-    let breadcrumbs = document.querySelectorAll('.global_navi .globalNavi-item-grn');
-    let outputs = [];
-    breadcrumbs.forEach(item=>{
-        outputs.push(item.innerText);
+    // user view
+    let breadcrumbs;
+    let breadcrumbs_user = document.querySelectorAll('div.global_navi span.globalNavi-item-grn');
+    let breadcrumbs_system = document.querySelector('div.site-posotion-system-grn div.global_navi');
+    let breadcrumb_string;
 
-    });
-    let last_breadcrumb = document.querySelector('.global_navi .globalNavi-item-last-grn');
-    outputs.push(last_breadcrumb.innerText);
-    let breadcrumb_string = outputs.join(" > ");
-    prompt("Copy Breadcrumb to Clipboard Ctrl+C.", breadcrumb_string);
+    if (breadcrumbs_user.length > 0) {
+        breadcrumbs = breadcrumbs_user;
+        let outputs = [];
+        breadcrumbs.forEach(item => {
+            outputs.push(item.innerText);
+
+        });
+        let last_breadcrumb = document.querySelector('div.global_navi span.globalNavi-item-last-grn');
+        outputs.push(last_breadcrumb.innerText);
+        breadcrumb_string = outputs.join(" > ");
+        prompt("Copy Breadcrumb to Clipboard Ctrl+C.", breadcrumb_string);
+    }
+    // system view
+    else if (breadcrumbs_system) {
+        breadcrumb_string = breadcrumbs_system.innerText;
+        prompt("Copy Breadcrumb to Clipboard Ctrl+C.", breadcrumb_string);
+    }
 })();
 
 /* compress
-javascript:(()=>{let l=document.querySelectorAll(".global_navi .globalNavi-item-grn"),e=[];l.forEach(l=>{e.push(l.innerText)});let r=document.querySelector(".global_navi .globalNavi-item-last-grn");e.push(r.innerText);prompt("Copy Breadcrumb to Clipboard Ctrl+C.",e.join(" > "))})();
 
-
+javascript:(()=>{let e,l=document.querySelectorAll("div.global_navi span.globalNavi-item-grn"),r=document.querySelector("div.site-posotion-system-grn div.global_navi"),i;if(l.length>0){let t=[];(e=l).forEach(e=>{t.push(e.innerText)});let o=document.querySelector("div.global_navi span.globalNavi-item-last-grn");t.push(o.innerText),prompt("Copy Breadcrumb to Clipboard Ctrl+C.",i=t.join(" > "))}else r&&prompt("Copy Breadcrumb to Clipboard Ctrl+C.",i=r.innerText)})();
 
  */
