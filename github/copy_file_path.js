d9dc85136098de41bc55235df148980e04632f5b
tuyen-nguyen <tuyen-nguyen@cybozu.onmicrosoft.com>
2024-06-26 16:50:08 +0700

    update copy clipboard

--- github/copy_file_path.js
+++ github/copy_file_path.js
@@ -1,17 +1,29 @@
 javascript: (() => {
-    let file_paths = document.querySelectorAll("a.Link--primary");
-    let output = "";
-    file_paths.forEach(file_path => {
-        output += file_path.getAttribute("title") + ",\n";
+    function copyToClipboard(text) {
+        let input = document.createElement('textarea');
+        input.innerHTML = text;
+        document.body.appendChild(input);
+        input.select();
+        let result = document.execCommand('copy');
+        document.body.removeChild(input);
+        if (result) {
+            alert("Copy to Clipboard successfully!");
+        } else {
+            alert("Copy to Clipboard failed!");
+        }
+    }
+
+    let file_items = document.querySelectorAll("a.Link--primary");
+    let file_path = "";
+    let outputs = [];
+    file_items.forEach(file_item => {
+        file_path = file_item.getAttribute("title");
+        if (file_path != null) {
+            outputs.push(file_path);
+
+        }
     });
-    console.log(output);
-    prompt("Copy to Clipboard Ctrl+C.", output);
 
+    let output_string = outputs.join("\n");
+    copyToClipboard(output_string);
 })();
-
-
-/* compress
-
-
-
-*/
