b08a271ef5e022f9d35be882ddbfbcba1dd3704f
tuyen-nguyen <tuyen-nguyen@cybozu.onmicrosoft.com>
2024-08-29 18:07:50 +0700

    update soap api

--- soap-api/cbpapi_cloud.sh
+++ soap-api/cbpapi_cloud.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "------------------------------------------------------------------------------------------------"
 XmlFile=$1
-ApiUrl=https://tuyennguyen-dev.cybozu-dev.com/g/cbpapi/schedule/api.csp
-sed -e 's/%USER_NAME%/cybozu/g' ${XmlFile} > XML_REQUEST.xml
+source config.ini
+ApiUrl=${URL}/g/cbpapi/${MODULE}/api.csp
+echo "Api Url: ${ApiUrl}"
+echo "XmlFile: ${XmlFile}"
+echo "Response:"
+sed -e "s/%USER_NAME%/${USER_NAME}/g" -e "s/%PASSWORD%/${PASSWORD}/g" ${XmlFile} > XML_REQUEST.xml
 curl -H "Content-Type: application/soap+xml; charset=utf-8;" -d "@XML_REQUEST.xml" -X POST "${ApiUrl}"
